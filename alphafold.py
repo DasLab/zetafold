c44835c90293214893a26ca6b0ce5d363675f49d
Andy Watkins <andy.watkins2@gmail.com>
2018-10-25 12:37:21 -0400

    Revert "Updated to print_function for python3 compatibility. Added test_alphafold.py (invoke with "pytest test_alphafold.py") to separately invoke tests"
    This reverts commit 9fb4df5161eb1badd4f06d71ceb4cebc100e800d.

--- alphafold.py
+++ alphafold.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python
-from __future__ import print_function
 import argparse
 
 C_init = 1
@@ -16,23 +15,23 @@ def initialize_zero_matrix( N ):
 
 def output_DP( tag, X, X_final = []):
     N = len( X )
-    print()
-    print("-----", tag, "-----")
+    print
+    print "-----", tag, "-----"
     for i in range( N ):
-        for q in range( i ): print('          ', end='') # padding to line up
+        for q in range( i ): print '          ', # padding to line up
         for j in range( N ):
-            print(' %9.3f' % X[i][(i+j) % N], end='')
-        if len( X_final ) > 0: print('==> %9.3f' % X_final[i], end='')
+            print ' %9.3f' % X[i][(i+j) % N],
+        if len( X_final ) > 0: print '==> %9.3f' % X_final[i],
         print
 
 def output_square( tag, X ):
     N = len( X )
-    print()
-    print("-----", tag, "-----")
+    print
+    print "-----", tag, "-----"
     for i in range( N ):
         for j in range( N ):
-            print(' %9.3f' % X[i][j], end='')
-        print()
+            print ' %9.3f' % X[i][j],
+        print
 
 def partition( sequences, circle = False ):
     if isinstance( sequences, str ): sequence = sequences
@@ -127,14 +126,14 @@ def partition( sequences, circle = False ):
     # stringent test that partition function is correct:
     for i in range( N ): assert( abs( ( Z_final[i] - Z_final[0] ) / Z_final[0] ) < 1.0e-5 )
 
-    print('sequence =', sequence)
+    print 'sequence =', sequence
     cutpoint = ''
     for i in range( N ):
         if is_cutpoint[ i ]: cutpoint += 'X'
         else: cutpoint += ' '
-    print('cutpoint =', cutpoint)
-    print('circle   = ', circle)
-    print('Z =',Z_final[0])
+    print 'cutpoint =', cutpoint
+    print 'circle   = ', circle
+    print 'Z =',Z_final[0]
 
     return ( Z_final[0], bpp )
 
@@ -148,6 +147,46 @@ if __name__=='__main__':
     sequences = args.sequences;
     circle   = args.circle;
 
-    (Z, bpp ) = partition( sequences, circle )
+    def output_test( Z, Z_ref = 0, bpp = [], bpp_idx= [], bpp_expected = 0):
+        print 'Z =',Z_ref,' [expected]'
+        assert( abs( (Z - Z_ref)/Z_ref )  < 1e-5 )
+        print
+        print 'bpp[0,4] = ',bpp[ bpp_idx[0] ][ bpp_idx[1] ]
+        print 'bpp[0,4] = ',bpp_expected,' [expected]'
+        assert( abs( (bpp[ bpp_idx[0] ][ bpp_idx[1] ] - bpp_expected)/bpp[ bpp_idx[0] ][ bpp_idx[1] ] )  < 1e-5 )
+        print
+
+
+    if sequences == None: # run tests
+        # test of sequences where we know the final partition function.
+        sequence = 'CAAAGAA'
+        (Z, bpp) = partition( sequence, circle = True )
+        output_test( Z, C_init  * (l**7) * (1 + C_init_BP / Kd_BP ) / C_std, \
+                     bpp, [0,4], (C_init**2 * (l**3) * l_BP/ Kd_BP) / ( C_init * (l**4) + C_init**2 * (l**3) * l_BP/ Kd_BP) )
+
+        sequence = 'CAG'
+        (Z, bpp) = partition( sequence )
+        output_test( Z, 1 + C_init * l**2 / Kd_BP, \
+                     bpp, [0,2], (C_init * l**2/Kd_BP)/( 1 + C_init * l**2/Kd_BP ) )
+
+        sequences = ['C','G']
+        (Z, bpp) = partition( sequences ) # note that Z sums over only base pair (not dissociated strands!)
+        output_test( Z, C_std * l / Kd_BP/l_BP, \
+                     bpp, [0,1], 1.0 )
+
+        sequences = ['GC','GC']
+        (Z, bpp) = partition( sequences )
+        output_test( Z, (C_std/Kd_BP)*(l/l_BP)*(2 + l*l_BP*C_init/Kd_BP ), \
+                     bpp, [0,3], (1 + l*l_BP*C_init/Kd_BP )/(2 + l*l_BP*C_init/Kd_BP ) )
+
+        sequence = 'CAGGC'
+        (Z, bpp) = partition( sequence ) # note that Z sums over only base pair (not dissociated strands!)
+        output_test( Z, 1+C_init*l**2/Kd_BP * ( 2 + l ), \
+                     bpp, [0,2], C_init*l**2/Kd_BP /(  1+C_init*l**2/Kd_BP * ( 2 + l )) )
+
+
+
+    else:
+        (Z, bpp ) = partition( sequences, circle )
 
 
