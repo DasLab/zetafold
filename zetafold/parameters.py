3a7a24ff2dcdf6a31e723eff866516eab7362c6b
Rhiju Das <rhiju@stanford.edu>
2018-12-03 14:48:25 -0800

    zetafold_v0.15.params moved to txt file

--- zetafold/parameters.py
+++ zetafold/parameters.py
@@ -22,12 +22,12 @@ def get_params( params = None, suppress_all_output = False ):
     params_object = None
     if isinstance(params,AlphaFoldParams): return params
     elif params == None or params =='': params_object = get_latest_params()
-    elif params == 'minimal':         params_object = get_params_from_file( 'minimal' )
-    elif params == 'v0.1':  params_object = get_params_from_file( 'zetafold_v0.1' )
-    elif params == 'v0.15': params_object = get_params_v0_15( AlphaFoldParams() )
-    elif params == 'v0.16': params_object = get_params_v0_16( AlphaFoldParams() )
-    elif params == 'v0.17': params_object = get_params_v0_17( AlphaFoldParams() )
-    elif params == 'v0.171': params_object = get_params_from_file( 'zetafold_v0.171' )
+    elif params == 'minimal': params_object = get_params_from_file( 'minimal' )
+    elif params == 'v0.1':    params_object = get_params_from_file( 'zetafold_v0.1' )
+    elif params == 'v0.15':   params_object = get_params_from_file( 'zetafold_v0.15' )
+    elif params == 'v0.16':   params_object = get_params_v0_16( AlphaFoldParams() )
+    elif params == 'v0.17':   params_object = get_params_v0_17( AlphaFoldParams() )
+    elif params == 'v0.171':  params_object = get_params_from_file( 'zetafold_v0.171' )
     else: print('unrecognized params requested: ', params)
     if not suppress_all_output: print('Parameters: ', params_object.name, ' version', params_object.version)
     return params_object
@@ -217,57 +217,3 @@ def get_params_v0_16( params ):
 
     return params
 
-
-# Parameters developed before extensive optimization
-def get_params_v0_15( params ):
-    # Starting to make use of train_zetafold.py on P4-P6.
-    params.name     = 'zetafold'
-    params.version  = '0.15'
-
-    params.min_loop_length = 3
-
-    # Seven parameter model
-    dG_init = +4.09 # Turner 1999, kcal/mol
-    Kd_CG = 1.0 * math.exp( dG_init/ KT_IN_KCAL)
-    dG_terminal_AU = 0.5 # Turner 1999, kcal/mol -- NUPACK
-    #Kd_AU = 10.0**5.40731537 # 255000
-    Kd_GU = 10.0**3.5863221 # 4000
-    Kd_AU = 10.0**5.40731537 # 255000
-    Kd_GU = 10.0**3.5863221 # 4000
-
-    dG_CG_CG = -3.30 # Turner 1999 5'-CC-3'/5'-GG-3', kcal/mol
-    params.C_eff_stacked_pair = math.exp( -dG_CG_CG / KT_IN_KCAL ) * Kd_CG # about 10^5
-
-    # From nupack rna1999.params
-    #>Multiloop terms: ALPHA_1, ALPHA_2, ALPHA_3
-    #>ML penalty = ALPHA_1 + s * ALPHA_2 + u *ALPHA_3
-    #>s = # stems adjacent to ML, u = unpaired bases in ML
-    # 340   40    0
-    #>AT_PENALTY:
-    #>Penalty for non GC pairs that terminate a helix
-    #  50
-    dG_bulge = 3.4 # bulge cost is roughly 3-4 kcal/mol
-    dG_multiloop_stems = 0.40 # in kcal/mol
-    dG_multiloop_unpaired = 0.0 #0.40 # in kcal/mol -- ZERO in NUPACK -- fudging here.
-    #params.C_init = 1.0 * math.exp( -dG_bulge / KT_IN_KCAL )
-    params.C_init = 10** 1.55034499 # radically different
-
-    params.l = math.exp( dG_multiloop_unpaired / KT_IN_KCAL )
-    params.l_BP = math.exp( dG_multiloop_stems/KT_IN_KCAL ) / params.l
-
-    setup_base_pair_type(params, 'C', 'G', Kd_CG )
-    setup_base_pair_type(params, 'A', 'U', Kd_AU )
-    setup_base_pair_type(params, 'G', 'U', Kd_GU )
-
-    #Kd_GA = Kd_AU*100000  # fudge factor to make GA weaker.
-    #base_pair_types.append( BasePairType( 'G', 'A', Kd_GA ) ) # totally made up
-    #Kd_AA = Kd_AU * 40 # fudge factor to make GU weaker.
-    #base_pair_types.append( BasePairType( 'A', 'A', Kd_AA ) ) # totally made up
-
-    # turn off coax!?
-    params.K_coax = 0.0
-    params.l_coax = 1.0
-
-    update_C_eff_stack( params, params.C_eff_stacked_pair )
-
-    return params
