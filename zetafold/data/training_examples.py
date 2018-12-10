57bde8ddc873abb5a2bac1e0c3a90d4788cbfa1a
Rhiju Das <rhiju@stanford.edu>
2018-12-10 15:00:16 -0800

    read training examples from disk (favorites)

--- zetafold/data/training_examples.py
+++ zetafold/data/training_examples.py
@@ -16,17 +16,45 @@ training_sets = {}
 training_set_names = []
 
 def read_in_training_examples( training_example_file ):
-    return
+    fid = open( training_example_file, 'r' )
+    line = fid.readline()
+    while line:
+        name = line[:-1]
+        line = fid.readline()
+        sequence  = line[:-1]
+        line = fid.readline()
+        structure = line[:-1]
+        line = fid.readline()
+        if line and len(line[:-1].replace(' ','')) > 0:
+            force_base_pairs = line[:-1]
+            line = fid.readline()
+        else:
+            force_base_pairs = None
+        all_training_examples[ name ] = TrainingExample( name, sequence, structure, force_base_pairs )
+        line = fid.readline()
+    fid.close()
 
 def read_in_training_sets( training_set_file ):
-    return
+    fid = open( training_set_file, 'r' )
+    line = fid.readline()
+    while line:
+        training_set_name = line[:-1]
+        line = fid.readline()
+        training_examples  = line[:-1].split(' ')
+        line = fid.readline()
+        training_set_names.append( training_set_name )
+        training_sets[ training_set_name ] = training_examples
+        line = fid.readline()
+    fid.close()
 
 data_dir = os.path.dirname( os.path.abspath(__file__) )
 
 training_example_files = glob( data_dir + '/*.examples.txt' )
+training_example_files.sort()
 for training_example_file in training_example_files: read_in_training_examples( training_example_file )
 
 training_set_files = glob( data_dir + '/*.sets.txt' )
+training_set_files.sort()
 for training_set_file in training_set_files: read_in_training_sets( training_set_file )
 
 # contrafold's training sets, based off RFAM
