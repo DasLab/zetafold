6a0cf03f0e4c4e0349adc117eabc09c68c47bfc4
Rhiju Das <rhiju@stanford.edu>
2018-12-21 09:17:02 -0800

    whoa fixing major bug in assert_equal

--- zetafold/util/assert_equal.py
+++ zetafold/util/assert_equal.py
@@ -1,6 +1,6 @@
 def assert_equal( x, y, tolerance = 1.0e-5 ):
     if abs(y) > 0:
-        assert( abs( x - y )/ y  < tolerance )
+        assert( abs( x - y )/ abs(y)  < tolerance )
     else:
         assert( abs( x ) == 0 )
     return
