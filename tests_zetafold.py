2c4382f90af291be0e22ad94ead61f4b268ba5cc
Andy Watkins <andy.watkins2@gmail.com>
2019-01-23 11:50:14 -0500

    Remove commented line to start PR

--- tests_zetafold.py
+++ tests_zetafold.py
@@ -3,7 +3,6 @@ from __future__ import print_function
 
 import argparse
 
-#from zetafold.output_helpers import *
 from zetafold.partition import *
 from zetafold.util.output_util import *
 from zetafold.parameters import get_params_from_file
